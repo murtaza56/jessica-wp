d01eabad68131501dba44495b9fd03cf6ed3a539
Angela J. Holden <angelajholden@gmail.com>
2016-10-01 07:25:58 -0500

    markup for the slider

--- includes/home-slider.php
+++ includes/home-slider.php
@@ -1,7 +1,21 @@
-<section>
+<section class="jessica-slider">
   <ul class="rslides">
-    <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slides-1.jpg"></li>
-    <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slides-2.jpg"></li>
-    <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slides-3.jpg"></li>
+
+  	<?php
+			$args = array(
+				'post_type' => 'jessica_slider',
+				'post_status' => 'publish',
+				'order' => 'desc',
+				'showposts' => 5
+			);
+			$post_loop = new WP_Query( $args );
+			while ( $post_loop->have_posts() ) : $post_loop->the_post(); ?>
+
+				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+				<li style="background-image:url(<?php echo $image['0']; ?>);"></li>
+
+			<?php	endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
   </ul>
 </section>
